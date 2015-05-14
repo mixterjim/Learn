96851aea65ae87a2ea077429fca1fbf9900b6b39
Jim <mr.jim.test@gmail.com>
2015-05-14 21:55:26 +0800

    Hello Python
    The first Python code.

---
+++ Python/hello world.py
@@ -0,0 +1,12 @@
+﻿name = raw_input('Input you name:\n')   #Input name
+age = input('Input you age:\n')
+if age > 17:
+    call = 'Mr.'
+else:
+    call = 'Little '
+print "Hi",call+name+",Let's say","hello","world!","\t",r"\\"+"I don't you gender"+"\\\\"
+''''How to call user
+    print hello world'''''
+print decode("utf8")'''Copyright © Jim
+Powered by python
+Thanks for Github'''
