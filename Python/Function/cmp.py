32b77827a3e26b4d804fc2f0e44f20ea5e3cb6c6
Jim <mr.jim.test@gmail.com>
2015-05-19 20:46:26 +0800

    another method
    "-" to ">"

--- Python/Function/cmp.py
+++ Python/Function/cmp.py
@@ -2,7 +2,7 @@ print cmp(1,2),cmp(2,1),cmp(1,1)
 def fake_cmp(x,y):
     if x - y < 0:
         return -1
-    elif x - y > 0:
+    elif x > y:
         return 1
     return 0
 print fake_cmp(1,2),fake_cmp(2,1),fake_cmp(1,1)
