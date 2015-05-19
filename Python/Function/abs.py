13a226ac529e90f26f059ac375043fe60c1ec007
Jim <mr.jim.test@gmail.com>
2015-05-19 21:24:58 +0800

    Error message
    Add Error message

--- Python/Function/abs.py
+++ Python/Function/abs.py
@@ -1,6 +1,8 @@
 print abs(1)
 print abs(-2)
 def fake_abs(x):
+    if not isinstance(x, (int, float)):    #Error
+        raise TypeError('bad operand type')
     if x < 0:
         x = -x
     return x
