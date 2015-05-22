54623179880f3370d84e00a1bd41c4e7c8add94c
Jim <mr.jim.test@gmail.com>
2015-05-22 22:37:19 +0800

    Some idea

--- Python/Temp.py
+++ Python/Temp.py
@@ -1,4 +1,17 @@
+import os
+[d for d in os.listdir('.')]
 'ABCDEFG'[:3]
 'ABC'
 'ABCDEFG'[::2]
 'ACEG'
+g = (x * x for x in range(10))
+print g.next(),g.next(),g.next(),g.next()
+for n in g:
+    print n
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print b
+        a, b = b, a + b
+        n = n + 1
+fib(6)
