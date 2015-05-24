0494618c58569bab5a968d5dd6821f70a07380d9
Jim <mr.jim.test@gmail.com>
2015-05-24 15:06:10 +0800

    Add filter

---
+++ Python/Function/filter.py
@@ -0,0 +1,11 @@
+l = range(101)
+def even(i):
+    return i % 2 == 0
+print filter(even, l)
+def fake_filter(function, list):
+    list_new=[]
+    for n in list:
+        if function(n) is True:
+            list_new.append(n)
+    return list_new
+print fake_filter(even, l)
