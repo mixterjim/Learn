ef90c45b6658897450f527f235bb77da38bdf41d
Jim <mr.jim.test@gmail.com>
2015-05-24 10:27:33 +0800

    Add map

---
+++ Python/Function/map.py
@@ -0,0 +1,11 @@
+l = [0,1,2,3,4,5,6,7,8,9,]
+def f(x):
+    return x**2
+print map(f,l)
+def fake_map(function,list):
+    list_new = []
+    for n in list:
+        i = function(n)
+        list_new.append(i)
+    return list_new
+print fake_map(f,l)
