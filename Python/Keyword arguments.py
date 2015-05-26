6c5e29ace3df669517d9b29eb247b3f40add2bec
Jim <mr.jim.test@gmail.com>
2015-05-26 18:36:20 +0800

    Add Keyword arguments
    Function parameters

---
+++ Python/Keyword arguments.py
@@ -0,0 +1,11 @@
+def sum(*number):
+    sum = 0
+    for n in number:
+        sum = sum + n
+    return sum
+print sum(),sum(1),sum(1,2)    #You can input any parameters
+l = [1,2,3]
+print sum(*l)    #sum(l[0], l[1], l[2])
+def student(name,age,**other):
+    print 'Name:',name,'Age:',age,'Other:',other
+student('Jim', 18, City='New York', Job='Student')
