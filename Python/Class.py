48cd4427f02d8b7078497c1c46c3ca9ab0328550
Jim <mr.jim.test@gmail.com>
2015-06-01 21:41:34 +0800

    Fix Bug

--- Python/Class.py
+++ Python/Class.py
@@ -2,17 +2,22 @@ class Student(object):
     def __init__(self, name, score):
         self.name = name
         self.score = score
-    def print_score(self):
-        print '%s: %s' % (self.name, self.score)
+    def get_score(self):
+        return '%s: %s' % (self.name, self.score)
     def get_grade(self):
         if self.score >= 90:
-            print 'A'
+            return 'A'
         elif self.score >= 60:
-            print 'B'
+            return 'B'
         else:
-            print 'C'
-bart = Student('Bart', 59)
-lisa = Student('Lisa', 0)
-lisa.__init__('Lisa', 87)
-bart.print_score()+bart.get_grade()
-lisa.print_score()
+            return 'C'
+Bart = Student('Bart', 59)
+Lisa = Student('Lisa', 0)    #Defined lisa
+Lisa.__init__('Lisa', 87)
+Jim = Student('Jim', 0) 
+Jim.score = 100
+print '%s  %s' % (Bart.get_score(), Bart.get_grade())
+name = Lisa
+print '%s  %s' % (name.get_score(), name.get_grade())
+name = Jim
+print '%s  %s' % (name.get_score(), name.get_grade())
