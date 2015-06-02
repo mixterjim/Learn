48261ef8f52c442277db84dae08340c787165aa7
Jim <mr.jim.test@gmail.com>
2015-06-02 23:20:26 +0800

    Add Protected

---
+++ Python/Class Protected.py
@@ -0,0 +1,18 @@
+class Students(object):
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+    def print_name(self):
+        print self.__name
+    def print_score(self):
+        print self.__score
+    def set_Student(self, name, score):
+        self.__name = name
+        self.__score = score
+Jim = Students('Jim',100)
+#Jim.__name
+Students.print_name(Jim)
+Students.print_score(Jim)
+#Ben.__name = "Ben"
+Ben = Students('Ben',66)
+print Ben._Students__name,Ben._Students__score    #Do not use this
--- Python/Partial function.py
+++ Python/Partial function.py
@@ -5,4 +5,4 @@ import functools
 int2_f = functools.partial(int, base=2)    #def
 print int2_f('10010')    #print int('10010', base=2)
 list = functools.partial(max, 18)    #get the max number
-print list(1,2,3)    #print max(18,1,2,3)
+print list(1,2,3)    #print the max in (18,1,2,3)
