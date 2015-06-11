0510a8971b501df0c5c44d477032e97c649241cc
Jim <mr.jim.test@gmail.com>
2015-06-11 22:55:20 +0800

    Completing

--- Python/20 Property.py
+++ Python/20 Property.py
@@ -9,4 +9,21 @@ class Student(object):
         self.score = value
 s = Student()
 s.set_score(100)
+#s.set_score(-1)
 print s.get_score()
+#-----------------------------#
+class Student(object):
+    @property
+    def score(self):
+        return self._score
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+s = Student()
+s.score = 99
+#s.score = 101
+print  s.score
