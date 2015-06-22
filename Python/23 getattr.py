0eb8fff9bfadc32e1f6a0fe64d1dcd019afb0b4d
Jim <mr.jim.test@gmail.com>
2015-06-22 23:10:56 +0800

    Chain

--- Python/23 getattr.py
+++ Python/23 getattr.py
@@ -6,3 +6,13 @@ class Student(object):
             return 0
 print 'Name:',Student().name
 print 'Score:',Student().score
+
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+    def __str__(self):
+        return self._path
+    __repr__ = __str__
+print Chain().status.user.timeline.list
