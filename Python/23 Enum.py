1dc481aba23e95688cb47b62941779c6154e4235
Jim <mr.jim.test@gmail.com>
2015-06-25 23:31:24 +0800

    Add Enum

---
+++ Python/23 Enum.py
@@ -0,0 +1,16 @@
+Jan = 1
+FEB = 2
+MAR = 3
+APR = 4
+MAY = 5
+JUN = 6
+JUL = 7
+AUG = 8
+SEP = 9
+OCT = 10
+NOV = 11
+DEC = 12
+from enum import Enum
+Month = Enum('Month', ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'))
+for name, member in Month.__members__.items():
+    print (name,'=>',member,',',member.value)
