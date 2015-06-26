41ea5eb5e8e2e19545070379e9215f0ec961626a
Jim <mr.jim.test@gmail.com>
2015-06-26 23:27:06 +0800

    Add Type

---
+++ Python/24 Type.py
@@ -0,0 +1,8 @@
+class Hello(object):
+    def hello(self, name='world'):
+        print ('Hello, %s.' % name)
+#Save as hello.py
+from hello import Hello
+Hello().hello()
+print (type(Hello))
+print (type(type(h))
