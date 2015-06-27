9803a1167eaf7a6a5f982936228ad7ff556a6883
Jim <mr.jim.test@gmail.com>
2015-06-27 22:32:30 +0800

    Finish Type
    type fn

--- Python/24 Type.py
+++ Python/24 Type.py
@@ -2,7 +2,16 @@ class Hello(object):
     def hello(self, name='world'):
         print ('Hello, %s.' % name)
 #Save as hello.py
-from hello import Hello
-Hello().hello()
+#from hello import Hello
+h = Hello()
+h.hello()
 print (type(Hello))
-print (type(type(h))
+print (type(h))
+print "----------------------------"
+def fn(self, name='world'):
+    print ('Hi, %s.' % name)
+Hi = type('Hi',(object,), dict(hello=fn)) #def Hi class
+g=Hi()
+g.hello()
+print (type(Hi))
+print (type(g))
