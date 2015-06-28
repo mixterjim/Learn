e15f5099a48ffd101d5574fb68899fc747b6a252
Jim <mr.jim.test@gmail.com>
2015-06-28 22:50:56 +0800

    Add Metaclass

---
+++ Python/25 Metaclass.py
@@ -0,0 +1,9 @@
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+class MyList(list):
+    __metaclass__ = ListMetaclass
+L = MyList()
+L.add(1)
+print L
