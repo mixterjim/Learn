49793d46c8104b29a16d9035f7d5185272e32f09
Jim <mr.jim.test@gmail.com>
2016-03-25 20:10:19 +0800

    update to python 3

--- Python/1 Boolean.py
+++ Python/1 Boolean.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-n=input("n=")   #input -1,0,1
-if n>0 or n<0:
-    print "n≠0"
+n = int(input("n="))  # input -1,0,1
+if n != 0:
+    print("n≠0")
 else:
-    print "n=0"
-if not n>0 and not n==0:
-    print "n<0"
-elif n==0:
+    print("n=0")
+if not n > 0 and not n == 0:
+    print("n<0")
+elif n == 0:
     None
