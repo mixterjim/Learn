7d0e906a9e9f0ecc1a592370925d971e6a465ede
Jim <mr.jim.test@gmail.com>
2016-05-07 18:17:35 +0800

    New Project
    What is the best game in your steam library.

---
+++ Python/Project/Steam/Good_Game.py
@@ -0,0 +1,25 @@
+from urllib import request
+from re import findall
+from pandas import DataFrame
+# urllib.request.urlopen(url, data=None, [timeout,]* cafile=None, capath=None, cadefault=False, context=None)
+html = request.urlopen('http://steamcommunity.com/id/mixterjim/games/?tab=all')
+data = html.read().decode('utf-8')
+id = findall(r'(?<=appid":)\d+', data)
+name = findall(r'(?<="name":").*?(?=","logo")', data)
+frame = DataFrame({'name': name, 'id': id, 'good': None, 'bad': None})
+i = 0
+score = {'good': [], 'bad': []}
+while i < len(frame):
+    req = request.Request('http://store.steampowered.com/app/%s/?cc=us' % (frame.id[i]))
+    req.add_header('Cookie', 'birthtime=-2209016279;')
+    html2 = request.urlopen(req)
+    data2 = str(html2.read())
+    good = findall(r'(?<=Positive </span> <span class="user_reviews_count">\().*?(?=\))', data2)
+    if good == []:
+        i += 1
+        continue
+    frame.good[i] = int(good[0].replace(',', ''))
+    bad = findall(r'(?<=Negative </span> <span class="user_reviews_count">\().*?(?=\))', data2)
+    frame.bad[i] = int(bad[0].replace(',', ''))
+    print(frame.sort('good'))
+    i += 1
