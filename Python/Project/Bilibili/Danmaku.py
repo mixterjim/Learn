7c6d1d4712c9e5fd330edb93b410747f8cb53576
Jim <mr.jim.test@gmail.com>
2016-05-17 10:33:35 +0800

    new function
    text in dict

--- Python/Project/Bilibili/Danmaku.py
+++ Python/Project/Bilibili/Danmaku.py
@@ -1,26 +1,38 @@
 import urllib.request
-room = input('请输入房间ID:')
+room = input('Room ID:')
 if len(room) == 0:
     room = 43508
-text = input('请输入弹幕内容:')
-if len(text) == 0:
-    text = '神级刷屏'
-num = 0  # 防止短时间发送相同内容弹幕会被屏蔽
-i = int(input('请输入循环次数(负数为无限循环):'))
-if len(i) == 0:
+text = input('Danmaku(Enter to exit):')
+data = []
+while text != '':
+    data.append(text)
+    text = input('Danmaku(Enter to exit):')
+if len(data) == 0:
+    data = ['Bilibili~~', ]
+num = 0
+k = 0
+i = input('Cycles(-1 to Endless loop):')
+if i == '':
     i = -1
+i = int(i)
 request = urllib.request.Request("http://live.bilibili.com/msg/send")
-file = open(r'Cookie.txt')
-cookie = file.read()
-# cookie = 'DedeUserID=???; DedeUserID__ckMd5=???; SESSDATA=???;'
+# file = open(r'Cookie.txt')
+# cookie = file.read()
+cookie = 'DedeUserID=???; DedeUserID__ckMd5=???; SESSDATA=???;'
 request.add_header('Cookie', cookie)
+print('Start')
 while i != 0:
-    dm = text + str(num)
-    data = 'color=16777215&fontsize=25&mode=1&msg=%s&rnd=1463234970&roomid=%s' % (
+    if k >= len(data):
+        k = 0
+    dm = str(data[k]+str(num))
+    post = 'color=16777215&fontsize=25&mode=1&msg=%s&rnd=1463234970&roomid=%s' % (
         dm, room)
-    data = data.encode('utf-8')
-    urllib.request.urlopen(request, data)
+    post = post.encode('utf-8')
+    print(dm)
+    # urllib.request.urlopen(request, post)
     if len(dm) >= 20:
         num = 0
     num += 1
     i -= 1
+    k += 1
+print('End')
