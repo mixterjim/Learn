f4874b1593956430adac7a022eaef04e02621b6d
Jim <mr.jim.test@gmail.com>
2016-05-18 13:25:35 +0800

    Random

--- Python/Project/Bilibili/Danmaku.py
+++ Python/Project/Bilibili/Danmaku.py
@@ -11,12 +11,15 @@ while text != '':
     text = input('Danmaku(Enter to exit):')
 if len(data) == 0:
     data = ['Bilibili~~', ]
-num = 0
 k = 0
 i = input('Cycles(-1 to Endless loop):')
 if len(i) == 0:
     i = -1
 i = int(i)
+start_hour = input('Start hour:')
+if len(start_hour) == 0:
+    start_hour = 0
+start_hour = int(start_hour)
 start_min = input('Start minute:')
 if len(start_min) == 0:
     start_min = 0
@@ -34,16 +37,14 @@ while i != 0:
         if k >= len(data):
             k = 0
             min = time.localtime()[4]
-        # dm = str(data[k] + str(num))
-        dm = data[k]
+        dm = str(data[k] + str(int(random.uniform(100, 1056))))
+        if len(dm) >= 20:
+            continue
         post = 'color=16777215&fontsize=25&mode=1&msg=%s&rnd=1463234970&roomid=%s' % (
             dm, room)
         post = post.encode('utf-8')
         # urllib.request.urlopen(request, post)
         print(dm)
-        if len(dm) >= 20:
-            num = 0
-        num += 1
         k += 1
         time.sleep(random.uniform(0.5, 1.5))
     print(time.localtime()[3], ':', time.localtime()[4], 'End')
