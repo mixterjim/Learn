ed8ebe48288328d22c7345fdbd9ff5b672c61db6
Jim <mr.jim.test@gmail.com>
2017-06-15 23:59:36 +0800

    Insertion Sort

---
+++ Python/CLRS/2.3-3_Insertion-Sort.py
@@ -0,0 +1,18 @@
+def INSERTION_SORT(list):
+    for n in range(1, len(list)):
+        for i in range(0, len(list) - 2):
+            if list[i] > list[i + 1]:
+                tmp = list[i]
+                list[i] = list[i + 1]
+                list[i + 1] = tmp
+    for i in range(0, len(list) - 2):
+        if list[-1] > list[i]:
+            k = i + 1
+        else:
+            break
+    tmp = list[k:-1]
+    list[k] = list[-1]
+    list[k + 1:] = tmp
+    print(list)
+A = [5, 2, 4, 6, 1, 3]
+INSERTION_SORT(A)
