870a17ca831a2f08d1a0647d136b834027b6517c
jim <mr.jim.test@gmail.com>
2017-06-26 22:06:07 +0800

    New

--- Python/CLRS/2.3-5_Binary_Search.py
+++ Python/CLRS/2.3-5_Binary_Search.py
@@ -1,9 +1,20 @@
+# import random
+# def random_int_list(start, stop, length):
+#     start, stop = (int(start), int(stop)) if start <= stop else (
+#         int(stop), int(start))
+#     length = int(abs(length)) if length else 0
+#     random_list = []
+#     for i in range(length):
+#         random_list.append(random.randint(start, stop))
+#     return random_list
+
+
 def CREATE_LIST(list):
     while True:
         key = input("Input:")
         if key == "":
             break
-        list.append(key)
+        list.append(int(key))
 
 
 def SELECTION_SORT(list):
@@ -17,11 +28,28 @@ def SELECTION_SORT(list):
 
 
 def BINARY_SEARCH(list, value):
-    pass
-
-A = [1, 2, 5, 4, 3, 0]
-# CREATE_LIST(A)
+    q = 0
+    p = len(list)
+    r = int((p - q) / 2)
+    while True:
+        if len(list[q:p]) == 1 and list[q + r] != value:
+            print("NIL")
+            break
+        if list[q + r] == value:
+            print(q + r)
+            break
+        if list[q + r] > value:
+            p = p - r
+        if list[q + r] < value:
+            q = q + r
+        r = int((p - q) / 2)
+A = []
+# A = [1, 2, 5, 4, 3, 0, 6, 8, 9]
+# A = random_int_list(1, 100000, 100)
+CREATE_LIST(A)
 SELECTION_SORT(A)
 print(A)
-v = input("Find:")
+v = int(input("Find:"))
+# for n in range(0, len(A)):
+#     v = A[n]
 BINARY_SEARCH(A, v)
