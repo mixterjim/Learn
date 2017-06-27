e8c2130a5544fad511ea727064565ff95093222f
jim <mr.jim.test@gmail.com>
2017-06-27 21:57:54 +0800

    New

--- Python/CLRS/2.3-7_Binary_Search.py
+++ Python/CLRS/2.3-7_Binary_Search.py
@@ -16,21 +16,22 @@ def BINARY_SEARCH(list, value):
         if len(list[q:p]) == 1 and list[q + r] != value:
             return False
         if list[q + r] == value:
-            return True
+            return q + r
         if list[q + r] > value:
             p = p - r
         if list[q + r] < value:
             q = q + r
         r = int((p - q) / 2)
+
+
+def PLUS(list, v):
+    for i in range(0, len(list)):
+        tmp = BINARY_SEARCH(list, v - list[i])
+        if tmp != False and tmp != i:
+            return True
+    return False
+
 A = [1, 2, 5, 4, 3, 0, 6, 8, 9]
 SELECTION_SORT(A)
 v = int(input('Input:'))
-k = 0
-for i in range(0, len(A)):
-    if BINARY_SEARCH(A, v - A[i]) != False:
-        k = 1
-        break
-if k == 0:
-    print(False)
-else:
-    print(True)
+print(PLUS(A, v))
