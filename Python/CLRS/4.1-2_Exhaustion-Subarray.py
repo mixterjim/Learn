6ed4abc363a5ed962ecf1eac5c1f41ded55612f2
Jim <mr.jim.test@gmail.com>
2017-07-10 11:04:59 +0800

    Update 4.1-2_Exhaustion-Subarray.py

--- Python/CLRS/4.1-2_Exhaustion-Subarray.py
+++ Python/CLRS/4.1-2_Exhaustion-Subarray.py
@@ -1,17 +1,20 @@
+def EXHAUSTION_FIND_MAXIMUM_SUBARRAY(list):
+    max = 0
+    left = 0
+    right = 0
+    for i in range(0, len(list)):
+        sum = list[i]
+        for j in range(i, len(list)):
+            if i == j:
+                sum = list[j]
+            else:
+                sum += list[j]
+            if sum > max:
+                max = sum
+                left = i
+                right = j
+    return left, right, max
+
 A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-max = 0
-left = 0
-right = 0
-n = 0
-for i in range(0, len(A)):
-    sum = A[i]
-    for j in range(i, len(A)):
-        if i == j:
-            sum = A[j]
-        else:
-            sum += A[j]
-        if sum > max:
-            max = sum
-            left = i
-            right = j
-print(left, right, max)
+
+print(EXHAUSTION_FIND_MAXIMUM_SUBARRAY(A))
