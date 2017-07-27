247d36a597f3043ce60622d6a273b3741b89c90f
jim <mr.jim.test@gmail.com>
2017-07-27 22:44:16 +0800

    Feat: add header

--- Python/Module/urllib/request.py
+++ Python/Module/urllib/request.py
@@ -1,7 +1,9 @@
 from urllib import request
 for id in range(1000000000, 1000000010):
     url = "http://tieba.baidu.com/p/%d" % id
-    html = request.urlopen(url).read()
+    req = request.Request(url)
+    req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
+    html = request.urlopen(req).read()
     file = open(str(id) + ".html", "wb")
     file.write(html)
     file.close()
