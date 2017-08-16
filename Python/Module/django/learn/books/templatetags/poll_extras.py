edbab3467068e5f0c057bfb4bdddf68593e41329
jim <mr.jim.test@gmail.com>
2017-08-16 23:54:32 +0800

    Feat(poll_extras): add tag

--- Python/Module/django/learn/books/templatetags/poll_extras.py
+++ Python/Module/django/learn/books/templatetags/poll_extras.py
@@ -1,17 +1,68 @@
 from django import template
 import datetime
-
+import re
 register = template.Library()
 
 
 class CurrentTimeNode(template.Node):
 
-    def __init__(self, format_string):
+    def __init__(self, format_string, var_name):
         self.format_string = str(format_string)
+        self.var_name = var_name
 
     def render(self, context):
         now = datetime.datetime.now()
-        return now.strftime(self.format_string)
+        context[self.var_name] = now.strftime(self.format_string)
+        return ''
+
+
+@register.tag(name="current_time")
+def do_current_time(parser, token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        msg = '%r tag requires arguments' % token.contents[0]
+        raise template.TemplateSyntaxError(msg)
+
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if m:
+        fmt, var_name = m.groups()
+    else:
+        msg = '%r tag had invalid arguments' % tag_name
+        raise template.TemplateSyntaxError(msg)
+
+    if not (fmt[0] == fmt[-1] and fmt[0] in ('"', "'")):
+        msg = "%r tag's argument should be in quotes" % tag_name
+        raise template.TemplateSyntaxError(msg)
+
+    return CurrentTimeNode(fmt[1:-1], var_name)
+
+
+# @register.simple_tag
+# def current_time(format_string):
+#     try:
+#         return datetime.datetime.now().strftime(str(format_string))
+#     except UnicodeEncodeError:
+#         return ''
+
+
+class UpperNode(template.Node):
+
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return output.upper()
+
+
+@register.tag(name="upper")
+def do_upper(parser, token):
+    nodelist = parser.parse(('endupper',))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
 
 
 def cut(value, arg):
@@ -23,14 +74,3 @@ register.filter('cut', cut)
 @register.filter
 def lower(value):  # Only one argument.
     return value.lower()
-
-
-@register.tag(name="current_time")
-def do_current_time(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, format_string = token.split_contents()
-    except ValueError:
-        msg = '%r tag requires a single argument' % token.split_contents()[0]
-        raise template.TemplateSyntaxError(msg)
-    return CurrentTimeNode(format_string[1:-1])
