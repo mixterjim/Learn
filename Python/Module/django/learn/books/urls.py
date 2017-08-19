985fcd2b55a664ce142283b2d158d3c3eb3f5b33
jim <mr.jim.test@gmail.com>
2017-08-20 00:21:35 +0800

    Feat: add csv

--- Python/Module/django/learn/books/urls.py
+++ Python/Module/django/learn/books/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from books.views import search, PublisherList, PublisherBookList
-
+from books.views import *
 
 urlpatterns = [
+    url(r'^$', PublisherList.as_view()),
+    url(r'^([\w\s]+)/$', PublisherBookList.as_view()),
     url(r'search/$', search),
-    url(r'^publishers/$', PublisherList.as_view()),
-    url(r'^publishers/([\w\s]+)/$', PublisherBookList.as_view()),
+    url(r'^down/csv/$', books_csv),
+    url(r'^down/pdf/$', books_pdf),
 ]
--- Python/Module/django/learn/books/views.py
+++ Python/Module/django/learn/books/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.generic import DetailView, ListView
 from books.models import Book, Publisher
+from io import BytesIO
+from reportlab.pdfgen import canvas
+import csv
 
 
 def search(request):
@@ -38,3 +42,37 @@ class PublisherBookList(ListView):
         context['publisher'] = self.publisher
         # Add in the publisher
         return context
+
+
+def books_csv(request):
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=book.csv'
+    # Create the HttpResponse object with the appropriate CSV header.
+    writer = csv.writer(response)
+    # Create the CSV writer using the HttpResponse as the "file."
+    writer.writerow(['Id', 'Book', 'Publisher'])
+    booklist = Book.objects.all()
+    for (id, book) in zip(range(len(booklist)), booklist):
+        publisher = book.publisher
+        writer.writerow([id + 1, str(book), str(publisher)])
+
+    return response
+
+
+def books_pdf(request):
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=book.pdf'
+    # Create the HttpResponse object with the appropriate PDF headers.
+    temp = BytesIO()
+    p = canvas.Canvas(temp)
+    # Create the PDF object, using the response object as its "file."
+    p.drawString(100, 100, 'Book')
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.showPage()
+    # Close the PDF object cleanly, and we're done.
+    p.save()
+    response.write(temp.getvalue())
+    return response
